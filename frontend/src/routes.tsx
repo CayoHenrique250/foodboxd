5bbbcab3adf76f7a382ce9b3d6a40d85364d7450
Cayo Henrique <cayoh250@gmail.com>
2025-11-15 13:49:27 -0300

    Configure Routes and Navigation (react-router-dom)

---
+++ frontend/src/routes.tsx
@@ -0,0 +1,79 @@
+/* eslint-disable react-refresh/only-export-components */
+
+import { createBrowserRouter, Outlet } from 'react-router-dom';
+
+const AppLayout = () => (
+  <div>
+    <h2>FoodBoxd (Menu Principal)</h2>
+    <hr />
+    <Outlet />
+  </div>
+);
+
+const LoginPage = () => <div>Tela de Login</div>;
+const CadastroPage = () => <div>Tela de Cadastro</div>;
+
+const DashboardPage = () => <div>Tela de Dashboard</div>;
+const RecomendacoesPage = () => <div>Tela de Recomendações</div>;
+const PesquisaPage = () => <div>Tela de Pesquisa</div>;
+const AmigosPage = () => <div>Tela de Amigos</div>;
+const PerfilPage = () => <div>Tela de Perfil</div>;
+const DiarioPage = () => <div>Tela de Diário</div>;
+const ListasPage = () => <div>Tela de Listas</div>;
+const MinhaListaPage = () => <div>Tela Minha Lista</div>;
+
+export const router = createBrowserRouter([
+
+  {
+    path: '/login',
+    element: <LoginPage />,
+  },
+  {
+    path: '/cadastro',
+    element: <CadastroPage />,
+  },
+
+  {
+    path: '/app',
+    element: <AppLayout />, 
+    children: [
+      {
+        path: 'dashboard', 
+        element: <DashboardPage />,
+      },
+      {
+        path: 'recomendacoes', 
+        element: <RecomendacoesPage />,
+      },
+      {
+        path: 'pesquisa', 
+        element: <PesquisaPage />,
+      },
+      {
+        path: 'amigos',
+        element: <AmigosPage />,
+      },
+      {
+        path: 'perfil',
+        element: <PerfilPage />,
+      },
+      {
+        path: 'diario',
+        element: <DiarioPage />,
+      },
+      {
+        path: 'listas', 
+        element: <ListasPage />,
+      },
+      {
+        path: 'minha-lista', 
+        element: <MinhaListaPage />,
+      },
+    ],
+  },
+  
+  {
+    path: '/',
+    element: <div>Carregando...</div>,
+  },
+]);
